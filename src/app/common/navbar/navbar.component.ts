021ec4ac3fab8bee73c28d4588bbbf2a0d485837
liamnoel <53420506+liamnoel@users.noreply.github.com>
2025-05-11 09:44:06 +0900

    remove unsed import

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -3,7 +3,6 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
